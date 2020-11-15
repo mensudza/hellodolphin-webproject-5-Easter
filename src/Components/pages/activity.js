d0f323be2594d2bb315cb9d6e65f1007af261e02
buratsakorn-bew <buratsakorn-boon@mail.kmutt.ac.th>
2020-11-16 01:00:30 +0700

    [update]organiz and activity

--- src/Components/pages/activity.js
+++ src/Components/pages/activity.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
+import ac from '../images/navbar/ac.png'
+import cloud3 from '../images/Pic/cloud3.png'
 
 const Container = styled.div`
   background-color: ${props => props.bgcolor};
@@ -13,8 +15,13 @@ export default class activity extends Component {
     render() {
         return (
             <div>
-                <Container bgcolor="blue">
-                    <h2>activity</h2>
+                <Container bgcolor="#A2E3FF">
+                    <div className="counter">
+                        <img className="cloud3" src={cloud3} /> 
+                        <img className="ac" src={ac} />
+                  
+                    </div>
+                
                 </Container>
             </div>
         )
--- src/Components/pages/organizer.js
+++ src/Components/pages/organizer.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
+import test2 from '../images/Pic/test2.png'
+import cloud2 from '../images/Pic/cloud2.png'
+import br from '../images/Pic/br.png'
+import rabbit4 from '../images/Mascot/rabbit4.png'
+import '../../index.css'
 
 const Container = styled.div`
   background-color: ${props => props.bgcolor};
@@ -13,8 +18,35 @@ export default class organizer extends Component {
     render() {
         return (
             <div>
-                <Container bgcolor="pink.">
-                    <h2>organizer</h2>
+                <Container bgcolor="#A2E3FF">
+                    <div className="counter">
+                        <img className="test2" src={test2} />
+                        <img className="rabbit4" src={rabbit4} />
+                        <img className="cloud2" src={cloud2} />
+                        <img className="cloud2l" src={cloud2} />
+                        <img className="br" src={br} />
+                        
+                    </div>
+                    
+                    <div className="footer">
+                        <h2>ฝ่าย Web design</h2>
+                        <div className="flex content-center">
+                            <p>นางสาวปณาลี ปาลศรี</p>
+                            <p>นางสาวพัทธ์ธีรา จงวัฒนานุกูล</p>
+                        </div>
+
+                        <h2> ฝ่าย Front-End</h2>
+                        <div className="flex content-center">
+                            <p>นายธาราดล เผือกเอี่ยม</p>
+                            <p>นายทศวรรษ สมตัว</p>
+                            <p>นางสาวบุรัสกร บุญมา</p>
+                        </div>
+
+                        <h2>ฝ่าย Infrastructure</h2>
+                        <p>นางสาวสุกัญญา ชินวิชา</p>
+
+
+                    </div>
                 </Container>
             </div>
         )
